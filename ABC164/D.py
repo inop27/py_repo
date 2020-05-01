fad8eaa9861c723b433ca0b52a74e80f926de9b8
inop27 <scarletinop@gmail.com>
2020-05-01 20:14:21 +0900

    ABC164のD問題解答修正

--- ABC164/D.py
+++ ABC164/D.py
@@ -1,16 +1,21 @@
-s = input()
+def main():
+    s = input()
 
-counter_list = [0] * 2019
-counter_list[0] = 1
-cumulative_sum_list = [0] * (len(s) + 1)
+    counter_list = [0] * 2019
+    counter_list[0] = 1
+    cumulative_sum_list = [0]
 
-for i in range(len(s)):
-    cumulative_sum_list[i + 1] = int(s[len(s)-i-1:len(s)]) % 2019
-    counter_list[cumulative_sum_list[i + 1]] += 1
+    for i in range(len(s)):
+        value = int(s[len(s)-i-1:len(s)]) % 2019
+        cumulative_sum_list.append(value)
+        counter_list[value] += 1
 
-ans = 0
+    ans = 0
 
-for i in counter_list:
-    ans += i * (i - 1) // 2
+    for i in counter_list:
+        ans += i * (i - 1) // 2
 
-print(ans)
+    print(ans)
+
+if __name__ == '__main__':
+    main()
